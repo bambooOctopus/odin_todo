2c0979943166290a6e0b158045b268146952bea9
bamboo_octopus <bamboooctopus@gmail.com>
2023-02-20 18:11:39 -0500

    load wallet instantiates and returns wallet obj

--- src/saveWalletModule.js
+++ src/saveWalletModule.js
@@ -1,11 +1,26 @@
+import { wallet } from "./walletModule";
+
 const saveWallet = (wallet) => {
     const walletString = JSON.stringify(wallet);
     localStorage.setItem("wallet", walletString);
 };
 
-const loadWallet = (wallet) => {
-    const walletString = localStorage.getItem("wallet");
-    const walletObj = JSON.parse(walletString);
+const loadWallet = (name) => {
+    const walletString = localStorage.getItem(name);
+    const walletParse = JSON.parse(walletString);
+    const walletArray = walletParse.walletArray;
+
+    const walletObj = wallet();
+
+    if (walletArray.length > 0) {
+        walletArray.forEach(proj => {
+            walletObj.addProject(proj)            
+        });
+    }
+
+    
+
+    
     return walletObj;
 
 };
